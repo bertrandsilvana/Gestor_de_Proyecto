b5d02c4f39aac7f8654383a58de29435714c443e
florencialarreategui <florencialarreategui@gmail.com>
2025-02-20 00:48:11 -0300

    se borran comentarios de clases

--- Clases/proyecto.php
+++ Clases/proyecto.php
@@ -16,45 +16,33 @@ class Proyecto {
         $this->id_proyecto = $id_proyecto;
         $this->nombre = $nombre;
         $this->descripcion = $descripcion;
-       // Comprobamos si $fechaInicio ya es un objeto DateTime
        $this->fechaInicio = $fechaInicio instanceof DateTime ? $fechaInicio : new DateTime($fechaInicio);
-        
-       // Comprobamos si $fechaFin ya es un objeto DateTime
        $this->fechaFin = $fechaFin instanceof DateTime ? $fechaFin : new DateTime($fechaFin);
-       
         $this->estado = $estado;
-        $this->tareas = $tareas;  // Inicializamos tareas
+        $this->tareas = $tareas; 
     }
 
     public function getId_proyecto() {
         return $this->id_proyecto;
     }
-
     public function getNombre() {
         return $this->nombre;
     }
-
     public function getDescripcion() {
         return $this->descripcion;
     }
-
     public function getFechaInicio() {
         return $this->fechaInicio;
     }
-
     public function getFechaFin() {
         return $this->fechaFin;
     }
-
     public function getEstado() {
         return $this->estado;
     }
-
     public function getTareas() {
         return $this->tareas;
     }
-
-   
     public function setId_proyecto($id_proyecto) {
         $this->id_proyecto = $id_proyecto;
     }
@@ -68,46 +56,36 @@ class Proyecto {
     }
 
     public function setFechaInicio($fechaInicio) {
-        // Comprobamos si $fechaInicio ya es un objeto DateTime
         $this->fechaInicio = $fechaInicio instanceof DateTime ? $fechaInicio : new DateTime($fechaInicio);
     }
 
     public function setFechaFin($fechaFin) {
-        // Comprobamos si $fechaFin ya es un objeto DateTime
         $this->fechaFin = $fechaFin instanceof DateTime ? $fechaFin : new DateTime($fechaFin);
     }
     public function setEstado($estado) {
         $this->estado = $estado;
     }
-
     public function setTareas($tareas) {
         $this->tareas = $tareas;
     }
-
-
-
     public function agregarTarea($tarea) {
         $this->tareas[] = $tarea;
     }
    
-  
-
- 
     public function eliminarTarea($id_tarea) {
         foreach ($this->tareas as $key => $tarea) {
             if ($tarea->getIdTarea() == $id_tarea) {
                 unset($this->tareas[$key]);
-                $this->tareas = array_values($this->tareas); // Reindexar el array
+                $this->tareas = array_values($this->tareas); 
                 break;
             }
         }
       
     }
 
-    // Convertimos el objeto Proyecto a un array para guardarlo en JSON (serialización)
     public function toArray() {
         $tareasArray = array_map(function($tarea) {
-            return $tarea->toArray();  // Convierte cada tarea a un array
+            return $tarea->toArray();  
         }, $this->tareas);
 
         return [
@@ -117,19 +95,17 @@ class Proyecto {
             'fechaInicio' => $this->fechaInicio->format('Y-m-d'),
             'fechaFin' => $this->fechaFin->format('Y-m-d'),
             'estado' => $this->estado,
-            'tareas' => $tareasArray,  // Incluye tareas como array
+            'tareas' => $tareasArray, 
         ];
     }
 
-        // Creamos un objeto Proyecto a partir de un array
         public static function fromArray($array) {
-            // Aseguramos que las fechas se convierten a DateTime solo si son cadenas
             $fechaInicio = $array['fechaInicio'] instanceof DateTime ? $array['fechaInicio'] : new DateTime($array['fechaInicio']);
             $fechaFin = $array['fechaFin'] instanceof DateTime ? $array['fechaFin'] : new DateTime($array['fechaFin']);
 
             $tareas = [];
             foreach ($array['tareas'] as $tareaArray) {
-                $tareas[] = Tarea::fromArray($tareaArray);  // Convierte cada tarea desde el array
+                $tareas[] = Tarea::fromArray($tareaArray);  
             }
 
             return new self(
@@ -139,7 +115,7 @@ class Proyecto {
                 $fechaInicio,
                 $fechaFin,
                 $array['estado'],
-                $tareas  // Pasa las tareas convertidas al objeto
+                $tareas  
             );
         }
     }
